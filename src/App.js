dbcc6fb8fbe8b798fa0cce2a4cab45edf14ff340
ellie <ellie.tms1371@gmail.com>
2020-06-01 22:33:12 +0100

    add favourite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React,{useState} from 'react';
 import BabyNames from './babyNames';
-import Search from './Search'
+import Search from './Search';
 import data from './data/babynames.json';
 import './App.css';
+import Favourites from './Favourites';
 
 function App() {
   const [inputName,setInputName] = useState("");
+  const [favouritesName,setFavouriteName] = useState("");
   const handleInput = (event) => {
-    console.log(event.target.value);
     setInputName(event.target.value);
   }
   const filteredNames = (data.filter((element) => {
     return (
       element.name.toLowerCase().includes(inputName.toLowerCase())
     )
-  }
-  )
-  )
+  }))
+
+
   return (
   <div className = "App" >
   <Search value={inputName} handleInput={handleInput}/>
+  <Favourites  />
   <BabyNames   filteredNames={filteredNames}/>
   </div> 
   );
---
+++ src/Favourites.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Favourites = () => {
+    return(
+       <h1 className="favourites m-5"><p className="mt-3 ml-4" style={{color:"blue"}}>Favo</p><p className="mt-3" style={{color:"red"}} >urites</p></h1>
+
+    )
+}
+
+export default Favourites;
