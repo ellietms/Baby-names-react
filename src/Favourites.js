6283e9505115996c31f0ae7db8f6fadb625f35ef
ellie <ellie.tms1371@gmail.com>
2020-06-01 22:38:21 +0100

    add css for favourites

--- src/Favourites.js
+++ src/Favourites.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Favourites = () => {
     return(
-       <h1 className="favourites m-5"><p className="mt-3 ml-4" style={{color:"blue"}}>Favo</p><p className="mt-3" style={{color:"red"}} >urites</p></h1>
+       <h1 className="favourites m-5"><p className="mt-3 ml-4" style={{color:"blue"}}>Favo</p><p className="mt-3" style={{color:"purple"}} >urites</p></h1>
 
     )
 }
